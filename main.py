404c5cd1936316872a0bc8c87e191cbacb1c012b
akmalrad-123 <akmalrad@gmail.com>
2025-08-02 22:58:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import datetime
 import config
 
 app = Flask(__name__)
-bot = Client(
-    "bot",
+bot = Client("media_bot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN
